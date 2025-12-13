c4267ee1ca55fdc0b6fb2ad7b8ea4842a0623b93
WuYou <1443207784@qq.com>
2025-12-13 14:51:46 +0800

    二阶段增加白化正则化项，算法主体采用R=20，IC=15的参数，尝试处理M1L1T1

--- main_USConvBSS_Exper.m
+++ main_USConvBSS_Exper.m
@@ -2,11 +2,11 @@
 clear; clc; close all;
 addpath('./Func');
 % 拓展因子
-% exFactor = 10;
+exFactor = 20;
 % 迭代容差
 Tolx = 1e-4;
 % 潜在成分个数
-% numCompo = 25;
+numCompo = 15;
 % 超声采样率
 fsampu = 2000;
 
@@ -17,11 +17,14 @@ end
 
 %% 读取数据并进行卷积盲源分离
 % for sub = [16]
-for exFactor = 10:10:40
-    for numCompo = 5:10:25
+
+% for exFactor = 10:10:40
+%     for numCompo = 5:10:25
+
 % for motion = 1%:2
 %     for trial = 1%:2
 motion = 1; trial = 1;
+
 %% TVI数据预处理
 disp('导入数据');
 % 导入TVI数据
@@ -50,14 +53,14 @@ parfor i = 1:size(TVIDataFilter, 3)
 end
 
 % 时间5-100Hz带通滤波
-% [Be2, Ae2] = butter(4, [5, 100]/fsampu*2);
-% for r = 1:size(TVIDataFilter, 1)
-%     parfor c = 1:size(TVIDataFilter, 2)
-%         tmp = squeeze(TVIDataFilter(r, c, :));
-%         tmp = filtfilt(Be2, Ae2, tmp);
-%         TVIDataFilter(r, c, :) = tmp;
-%     end
-% end
+[Be2, Ae2] = butter(4, [5, 100]/fsampu*2);
+for r = 1:size(TVIDataFilter, 1)
+    parfor c = 1:size(TVIDataFilter, 2)
+        tmp = squeeze(TVIDataFilter(r, c, :));
+        tmp = filtfilt(Be2, Ae2, tmp);
+        TVIDataFilter(r, c, :) = tmp;
+    end
+end
 
 % 对每一列降采样
 parfor i = 1:size(TVIDataFilter, 3)
@@ -236,7 +239,9 @@ DecompoResults.B2 = reshape(tmpB2, numRows, numCols)';
 % save(['./Data/experiment/24-06-21/UUS-iEMG/S1M1L' num2str(level) 'T' num2str(trial) '_USCBSS_compo' num2str(numCompo) '_' num2str(pp) '_2s1.mat'], 'DecompoResults', '-v7.3');
 save(['./Data/experiment/25-07-04/M' num2str(motion) 'L1T' num2str(trial) '_USCBSS_R' num2str(exFactor) 'compo' num2str(numCompo) '.mat'], 'DecompoResults', '-v7.3');
 disp('数据保存完成！');
+
 %     end
 % end
-    end
-end
+
+%     end
+% end
